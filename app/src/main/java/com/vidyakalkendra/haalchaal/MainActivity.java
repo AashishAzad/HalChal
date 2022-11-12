0ec4c8f7ccbe07ad0156665805bf8182784f0084
Aashish <aashishazad9@gmail.com>
2022-11-12 23:56:04 +0530

    theme change

--- app/src/main/java/com/vidyakalkendra/haalchaal/MainActivity.java
+++ app/src/main/java/com/vidyakalkendra/haalchaal/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
@@ -30,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
 
         newsType = findViewById(R.id.NewsTypes);
